ea17c9049f958f01308318f0d99faa4c66513bec
Daniel Lebu <dalebu@microsoft.com>
2016-04-11 10:30:32 -0700

    Exporting new methods: launchBrowser, launchServer.

--- src/simulate.js
+++ src/simulate.js
@@ -4,27 +4,40 @@ var cordovaServe = require('cordova-serve'),
     path = require('path'),
     simulateServer = require('taco-simulate-server');
 
-module.exports = function (opts) {
+var launchServer = function(opts) {
+    return simulateServer(opts, {
+        simHostRoot: path.join(__dirname, 'sim-host'),
+        node_modules: path.resolve(__dirname, '..', 'node_modules')
+    })
+};
+
+var launchBrowser = function(target, url) {
+    return cordovaServe.launchBrowser({ target: target, url: url });
+};
+
+var simulate = function(opts) {
     require('./server/server').attach(simulateServer.app);
 
     var target = opts.target || 'chrome';
     var simHostUrl;
 
-    return simulateServer(opts, {
-        simHostRoot: path.join(__dirname, 'sim-host'),
-        node_modules:  path.resolve(__dirname, '..', 'node_modules')
-    }).then(function (urls) {
-        simHostUrl = urls.simHostUrl;
-        return cordovaServe.launchBrowser({target: target, url: urls.appUrl});
-    }).then(function () {
-        return cordovaServe.launchBrowser({target: target, url: simHostUrl});
-    }).catch(function (error) {
-        // Ensure server is closed, then rethrow so it can be handled by downstream consumers.
-        simulateServer.server && simulateServer.server.close();
-        if (error instanceof Error) {
-            throw error;
-        } else {
-            throw new Error(error);
-        }
-    });
+    return launchServer(opts)
+        .then(function(urls) {
+            simHostUrl = urls.simHostUrl;
+            return launchBrowser(target, urls.appUrl);
+        }).then(function() {
+            return launchBrowser(target, simHostUrl);
+        }).catch(function(error) {
+            // Ensure server is closed, then rethrow so it can be handled by downstream consumers.
+            simulateServer.server && simulateServer.server.close();
+            if (error instanceof Error) {
+                throw error;
+            } else {
+                throw new Error(error);
+            }
+        });
 };
+
+module.exports = simulate;
+module.exports.launchBrowser = launchBrowser;
+module.exports.launchServer = launchServer;
